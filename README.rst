e5c0887cc095175237c19e5fb59231929483043a
Nolan Lum <nol888@gmail.com>
2011-12-04 02:42:14 -0500

    Add readme. Cleanup extraneous comments in settings.py

---
+++ README.rst
@@ -0,0 +1,16 @@
+=====================
+fBSD-Community-Portal
+=====================
+The unofficial FreeBSD Community Portal is being developed as a project
+for the Google Code-In event, and will (hopefully) allow simple communication
+and collaboration for whatever the FreeBSD organization wishes to use it for.
+
+------------
+Dependencies
+------------
+- Django Web Framework
+- django-social-auth_
+
+We use django-social-auth_ to interface with OpenID/OAuth providers.
+
+.. _django-social-auth: https://github.com/omab/django-social-auth
--- settings.py
+++ settings.py
@@ -29,12 +29,7 @@ LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
@@ -63,9 +58,6 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
@@ -79,11 +71,9 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 't53xqy+s_p6_ynce#y$hr75_1_)g+%f^v6aczhf%20a8lm8_ps'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -138,26 +128,3 @@ LINKEDIN_EXTRA_FIELD_SELECTORS=['']
 LOGIN_REDIRECT_URL           = '/'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
